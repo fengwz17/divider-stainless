4517e9ceccb5b54ab1a4ac5684150dce57a1f2cd
fengwz17 <fengwz17@outlook.com>
2023-08-25 14:49:39 +0800

    library UInt

---
+++ stain/src/main/scala/verify/libraryUInt/OtherUtil.scala
@@ -0,0 +1,36 @@
+package libraryUInt
+
+import stainless.lang._
+import stainless.collection._
+import stainless.equations._
+import stainless.annotation._
+import stainless.proof.check
+
+@opaque @library
+object range {
+  /* Range from start (inclusive) to until (exclusive) */
+  def apply(start: BigInt, until: BigInt): List[BigInt] = {
+    require(start <= until)
+    decreases(until - start)
+    if(until <= start) Nil[BigInt]() else Cons(start, range(start + 1, until))
+  } ensuring{(res: List[BigInt]) => res.size == until - start }
+}
+
+@opaque @library
+object max {
+  def apply(a: BigInt, b: BigInt): BigInt = {
+    if (a > b) a else b
+  }
+  def apply(ns: List[BigInt]): BigInt = {
+    require(ns.size > 0)
+    def f(n: BigInt, ns: List[BigInt]): BigInt = {
+      require(ns.size >= 0)
+      decreases(ns)
+      ns match {
+        case Cons(head, tail) => f(max(n, head), tail)
+        case Nil()            => n
+      }
+    }
+    f(ns.head, ns.tail)
+  }
+}
